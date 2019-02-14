4ca224062efb8598288524e34fc5f459cb37ccad
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-02-14 12:27:42 +0200

    Oop menu - Menüü kaart koos headeriga ja sisuga

--- menu/index.php
+++ menu/index.php
@@ -3,8 +3,22 @@ require_once 'conf.php';
 //loome vajalikud vaate objektid
 $mainTmpl = new Template('main');
 $contentTmpl = new Template('content');
+$cardTmpl = new Template('card');
+$cardHeaderTmpl = new Template('header');
+$cardDataTmpl = new Template('data');
+$listTmpl = new Template('list');
+
 
 $mainTmpl->set('title', 'Menu Application');
+
+$cardTmpl->set('card_header', $cardHeaderTmpl->parse());
+$cardTmpl->set('card_data', $cardDataTmpl->parse());
+$cardDataTmpl->add('dish_list', $listTmpl);
+
+$contentTmpl->add('cards', $cardTmpl->parse());
+$contentTmpl->add('cards', $cardTmpl->parse());
+$contentTmpl->add('cards', $cardTmpl->parse());
+$contentTmpl->add('cards', $cardTmpl->parse());
 $mainTmpl->set('content', $contentTmpl->parse());
 
 echo $mainTmpl->parse();
