2be43fc789e186f65215821b0e35ed96ef32df59
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-02-14 11:34:26 +0200

    Oop menu - Lisatud Content view

--- menu/index.php
+++ menu/index.php
@@ -1,12 +1,11 @@
 <?php
-
-
 require_once 'conf.php';
-
+//loome vajalikud vaate objektid
 $mainTmpl = new Template('main');
+$contentTmpl = new Template('content');
 
 $mainTmpl->set('title', 'Menu Application');
-$mainTmpl->set('content', 'Menu App Content');
+$mainTmpl->set('content', $contentTmpl->parse());
 
 echo $mainTmpl->parse();
 
